1ae7b4ed98e1b4548ac562aa3243d10615920697
Alex <alexBigBoss@users.noreply.github.com>
2015-10-02 09:19:54 -0500

    Update Connection.php

--- Login_PHP/Connection.php
+++ Login_PHP/Connection.php
@@ -1,18 +1,18 @@
 <?php
     class Connection {
         public function getConnection() {
-		    $host = "localhost";
-		    $dbName = "u651189321_bdrem";
-		    $root = "u651189321_fcfm";
-		    $password = "cdis123";
+	    $host = "localhost";
+	    $dbName = "u651189321_bdrem";
+	    $root = "u651189321_fcfm";
+	    $password = "cdis123";
 				
-		    try {
-				$connect = new PDO("mysql:host=$host;dbname=$dbName;", $root, $password);
-		    } catch (PDOException $ex) {
-				die("Failed to connect to the database: " . $ex->getMessage());
+	    try {
+		$connect = new PDO("mysql:host=$host;dbname=$dbName;", $root, $password);
+	    } catch (PDOException $ex) {
+		die("Failed to connect to the database: " . $ex->getMessage());
             }
 				
-		    return $connect;
+	    return $connect;
         }
     }
 ?>
